878b175c28f17bc1ee2f789a9c23996417031f60
mrunalbhonde <bhondemrunal0@gmail.com>
2024-07-22 13:39:14 +0530

    added model info

---
+++ backend/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model('User', UserSchema);
